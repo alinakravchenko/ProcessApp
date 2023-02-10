1cbc3120e66844ec074111f4271e4cc2c5f214fd
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2023-02-10 21:05:39 +0300

    Добавьте файлы проекта.

---
+++ ProcessApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace ProcessApp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+        public void UpdateProcessList()
+        {
+            Process[] processes = Process.GetProcesses();
+            listBox1.Items.Clear();
+            foreach(Process process in processes)
+            {
+                listBox1.Items.Add(process.ProcessName);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //пользователь задаёт сам время обновления списка
+            timer1.Interval = Int32.Parse(textBox1.Text)*1000;
+        }
+    }
+}
